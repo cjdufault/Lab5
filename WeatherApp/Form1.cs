5b28eb0b713f706cea2f02d320ae50990dfd61df
Connor Dufault <mx8434an@go.minnstate.edu>
2020-02-20 10:42:21 -0600

    added weather app

---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        string BaseUrl = "http://weather-csharp.herokuapp.com/";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetWeather_Click(object sender, EventArgs e)
+        {
+            btnGetWeather.Enabled = false; // to make sure no request is made while a request is in progress
+
+        }
+
+        private string GetWeatherText(string city, string state)
+        {
+            string requestUrl = String.Format("{0}text?city={1}&state={2}", BaseUrl, city, state);
+            string weatherText;
+
+            using (WebClient client = new WebClient())
+            {
+                weatherText = client.DownloadString(requestUrl);
+            }
+
+            return weatherText;
+        }
+    }
+}
