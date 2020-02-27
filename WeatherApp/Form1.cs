282c799b693660aa8190a482a5a4febfcb2ec5fa
Connor Dufault <mx8434an@go.minnstate.edu>
2020-02-27 10:06:56 -0600

    added guam and us virgin islands; for some reason American Samoa comes up as outside of the US

--- WeatherApp/Form1.cs
+++ WeatherApp/Form1.cs
@@ -17,14 +17,14 @@ namespace WeatherApp
     {
         readonly string BaseUrl = "http://weather-csharp.herokuapp.com/";
         readonly string[] States = {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
-                                    "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia",
+                                    "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Guam",
                                     "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
                                     "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
                                     "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
                                     "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
                                     "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", 
-                                    "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", 
-                                    "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" };
+                                    "South Carolina", "South Dakota", "Tennessee", "Texas", "US Virgin Islands", "Utah", "Vermont", 
+                                    "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"};
 
         public Form1()
         {
